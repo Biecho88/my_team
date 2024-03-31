3e02fe8bf7c44b53c64f0843a1c2e363b41af0d9
Piotr Bieszczad <biecho88@gmail.com>
2024-03-31 22:12:11 +0000

    Created data model for our_teams and migrated it

--- our_teams/models.py
+++ our_teams/models.py
@@ -3,14 +3,11 @@ from django.db import models
 
 class My_Team(models.Model):
     team_name = models.CharField(max_length=50)
-    team_category = models.CharField(max_length=50)
     
     
     def __str__(self):
         return self.team_name
 
-    def get_team_category(self):
-        return self.team_category
 
 
 class Team(models.Model):
@@ -19,8 +16,8 @@ class Team(models.Model):
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     position = models.CharField(max_length=50)
-    second_position = models.CharField(max_length=50)
-    health_conditions = models.CharField(max_length=50)
+    position_side = models.CharField(max_length=50)
+    health_conditions = models.CharField(max_length=100)
     comments = models.CharField(max_length=100)
 
 
