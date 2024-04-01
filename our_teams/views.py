9698e04ab524a88dc7e6665081e8513554531aa1
Piotr Bieszczad <biecho88@gmail.com>
2024-04-01 20:33:00 +0000

    Load data from models in to the page. Tested and works

--- our_teams/views.py
+++ our_teams/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from .models import Team
+
 
 def our_teams(request):
-    return render(request, 'our_teams.html', {})
+
+    """ A view to show all products, including sorting and search queries """
+
+    teams = Team.objects.all()
+
+    context = {
+        'teams': teams,
+    }
+
+    return render(request, 'our_teams.html', context)
